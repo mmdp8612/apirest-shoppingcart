d504a66a2f59e7510eba5158642109d9a6266d74
Martin Della Penna <mmdp8612@gmail.com>
2023-05-14 14:25:23 -0300

    Modificaciones

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.set("PORT", 8888);
+const port = process.env.PORT || 8888;
 
 app.use("/", require("./routes/index"));
 app.use("/api/productos", require("./routes/productos"));
 
-app.listen(app.get("PORT"), () => {
-    console.log(`Servidor escuchando el puerto ${app.get("PORT")}...`);
+app.listen(port, () => {
+    console.log(`Servidor escuchando el puerto ${port}...`);
 });
